0b1b70a4cb3976b3a4be9fe7cfe4c6bf340d668a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-24 13:32:21 +0530

    used context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import React from 'react';
 import ChakraWrap from './utils/ChakraWrap';
+import { StateContext } from './context/context';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 export default function App() {
 	return (
 		<ChakraWrap>
+			<Router>
+				<StateContext>
 
+				</StateContext>
+			</Router>
 		</ChakraWrap>
 	)
 }
