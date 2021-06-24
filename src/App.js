907ab4d9bfb10a62dc8bba14529d719d4c028857
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-24 13:27:49 +0530

    chakra ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
 import './App.css';
+import React from 'react';
+import ChakraWrap from './utils/ChakraWrap';
 
 export default function App() {
 	return (
-		<div>
+		<ChakraWrap>
 
-		</div>
+		</ChakraWrap>
 	)
 }
