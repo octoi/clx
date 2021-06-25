d9b669b6bd19cdc5041bae90d3a837f3487acd5c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-06-25 13:19:22 +0530

    detail

---
+++ src/components/detail/ProductDetails.js
@@ -0,0 +1,81 @@
+import React, { useRef, useState, useEffect } from 'react';
+import moment from 'moment';
+import useCtx from '../../context/useCtx';
+import { useHistory } from 'react-router-dom';
+import { ProductDetailContainer, CustomButton, Description, UserDetail } from './styledComponents';
+import { Button, Spinner } from '@chakra-ui/react';
+import { getUserData } from '../../firebase/helpers/firebaseUser';
+
+export default function ProductDetails({ product }) {
+	const [loading, setLoading] = useState(false);
+	const [seller, setSeller] = useState(null);
+	const { user } = useCtx();
+	const deleteBtn = useRef();
+	const history = useHistory();
+
+	useEffect(() => {
+		getUserData(product?.sellerEmail).then(userData => {
+			setSeller(userData);
+		})
+	}, [product]);
+
+	const deleteProduct = async () => {
+		const word = "Click To Confirm"
+
+		if (deleteBtn.current.innerText !== word) {
+			deleteBtn.current.innerText = word;
+			return;
+		}
+
+		setLoading(true);
+
+		await product?.ref.delete();
+		window.location.href = "/"
+
+	}
+
+	return (
+		<ProductDetailContainer>
+			<div>
+				<p>{product?.title}</p>
+				<p>₹{product?.price}</p>
+			</div>
+			<div>
+				{user.email === product?.sellerEmail ? (
+					<Button
+						size="lg"
+						as={CustomButton}
+						style={{ background: 'var(--dangerColor)' }}
+						onClick={deleteProduct}
+						ref={deleteBtn}
+					>
+						{loading ? <Spinner /> : "Delete Product"}
+					</Button>
+				) : (
+					<Button
+						size="lg"
+						as={CustomButton}
+						style={{ background: 'var(--primaryAccentColor)' }}
+						onClick={() => history.push(`/chat/${product?.user.email}`)}
+					>Contact Seller</Button>
+				)}
+				<section>
+					<span>{product?.location}</span><br />
+					<span>{moment(product?.date).fromNow()}</span>
+				</section>
+			</div>
+			<Description>{product?.description}</Description>
+			{seller && (
+				<UserDetail onClick={() => {
+					history.push(`/user/${seller.email}`)
+				}}>
+					<img src={seller.profile} alt={seller.name} />
+					<div>
+						<span className="name">{seller.name}</span>
+						<span>{seller.email}</span>
+					</div>
+				</UserDetail>
+			)}
+		</ProductDetailContainer>
+	)
+}
--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React from 'react'
+import CoverImages from '../components/detail/CoverImages';
+import ProductDetails from '../components/detail/ProductDetails';
+import { useFetchOneProduct } from '../firebase/helpers/fetchProduct';
+import { useParams } from 'react-router-dom';
+import { Flex, Spinner, useToast } from '@chakra-ui/react';
 
 export default function Detail() {
+	const { productId } = useParams(window.location.search);
+	const { product, loading } = useFetchOneProduct(productId);
+	const toast = useToast();
+
+	if (product?.message && !product?.status) {
+		toast({
+			title: "No such product",
+			duration: 5000,
+			isClosable: true,
+			position: "top-right",
+			status: "error"
+		});
+	}
+
 	return (
 		<div>
-
+			{loading && (
+				<Flex height="80vh" width="100%" alignItems="center" justifyContent="center">
+					<Spinner />
+				</Flex>
+			)}
+			{product?.status && (
+				<div>
+					<CoverImages images={product?.message.images} />
+					<ProductDetails product={product?.message} />
+				</div>
+			)}
 		</div>
 	)
 }
